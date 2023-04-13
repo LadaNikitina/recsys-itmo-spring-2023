4a30335e28420187c8d754d66c71737ac7c52e06
Ledneva Daria <da.led@mail.ru>
2023-04-13 16:52:33 +0300

    Update server.py

--- botify/botify/server.py
+++ botify/botify/server.py
@@ -11,6 +11,7 @@ from gevent.pywsgi import WSGIServer
 
 from botify.data import DataLogger, Datum
 from botify.experiment import Experiments, Treatment
+from botify.recommenders.my_recommender import MyRecommender
 from botify.recommenders.random import Random
 from botify.recommenders.sticky_artist import StickyArtist
 from botify.recommenders.toppop import TopPop
@@ -38,8 +39,7 @@ data_logger = DataLogger(app)
 
 # TODO Seminar 6 step 4: Upload tracks with diverse recommendations to redis DB
 catalog = Catalog(app).load(
-    app.config["TRACKS_CATALOG"], app.config["TOP_TRACKS_CATALOG"], app.config["TRACKS_WITH_DIVERSE_RECS_CATALOG"]
-)
+    app.config["TRACKS_CATALOG"], app.config["TOP_TRACKS_CATALOG"], app.config["TRACKS_WITH_DIVERSE_RECS_CATALOG"])
 catalog.upload_tracks(tracks_redis.connection, tracks_with_diverse_recs_redis.connection)
 catalog.upload_artists(artists_redis.connection)
 catalog.upload_recommendations(recommendations_redis.connection)
@@ -74,21 +74,27 @@ class NextTrack(Resource):
         args = parser.parse_args()
 
         # TODO Seminar 6 step 6: Wire RECOMMENDERS A/B experiment
-        treatment = Experiments.RECOMMENDERS.assign(user)
+        treatment = Experiments.MY_RECOMMENDER.assign(user)
         if treatment == Treatment.T1:
-            recommender = StickyArtist(tracks_redis.connection, artists_redis.connection, catalog)
-        elif treatment == Treatment.T2:
-            recommender = TopPop(tracks_redis.connection, catalog.top_tracks[:100])
-        elif treatment == Treatment.T3:
-            recommender = Indexed(tracks_redis.connection, recommendations_ub_redis.connection, catalog)
-        elif treatment == Treatment.T4:
-            recommender = Indexed(tracks_redis.connection, recommendations_redis.connection, catalog)
-        elif treatment == Treatment.T5:
-            recommender = Contextual(tracks_redis.connection, catalog)
-        elif treatment == Treatment.T6:
-            recommender = Contextual(tracks_with_diverse_recs_redis.connection, catalog)
+            recommender = MyRecommender(tracks_redis.connection, recommendations_redis.connection, catalog)
         else:
-            recommender = Random(tracks_redis.connection)
+            recommender = Contextual(tracks_redis.connection, catalog)
+
+        # treatment = Experiments.RECOMMENDERS.assign(user)
+        # if treatment == Treatment.T1:
+        #     recommender = StickyArtist(tracks_redis.connection, artists_redis.connection, catalog)
+        # elif treatment == Treatment.T2:
+        #     recommender = TopPop(tracks_redis.connection, catalog.top_tracks[:100])
+        # elif treatment == Treatment.T3:
+        #     recommender = Indexed(tracks_redis.connection, recommendations_ub_redis.connection, catalog)
+        # elif treatment == Treatment.T4:
+        #     recommender = Indexed(tracks_redis.connection, recommendations_redis.connection, catalog)
+        # elif treatment == Treatment.T5:
+        #     recommender = Contextual(tracks_redis.connection, catalog)
+        # elif treatment == Treatment.T6:
+        #     recommender = MyRecommender(tracks_redis.connection, my_recommendations_redis.connection, catalog)
+        # else:
+        #     recommender = Random(tracks_redis.connection)
 
         recommendation = recommender.recommend_next(user, args.track, args.time)
 
