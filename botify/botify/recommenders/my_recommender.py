ee7bc19ca446207544fd40e6687ab47270145f45
Ledneva Daria <da.led@mail.ru>
2023-04-13 22:04:43 +0300

    Add files via upload

---
+++ botify/botify/recommenders/my_recommender.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from .random import Random
+from .recommender import Recommender
+import random
+import joblib
+
+
+def norm(x):
+  return (x - x.mean(axis=0)) / x.std(axis=0)
+
+class MyRecommender(Recommender):
+    """
+    Recommend tracks closest to the previous one.
+    Fall back to the random recommender if no
+    recommendations found for the track.
+    """
+
+    def __init__(self, tracks_redis, recommendations_redis, catalog):
+        self.recommendations_redis = recommendations_redis
+        self.tracks_redis = tracks_redis
+        self.fallback = Random(tracks_redis)
+        self.catalog = catalog
+        self.user_embs = np.load('/app/botify/recommenders/user_embeddings.npy')
+        self.context_embs = np.load('/app/botify/recommenders/context_embeddings.npy')
+        self.track_embs = np.load('/app/botify/recommenders/track_embeddings.npy')
+
+    def recommend_next(self, user: int, prev_track: int, prev_track_time: float) -> int:
+        previous_track = self.tracks_redis.get(prev_track)
+        if previous_track is None:
+            return self.fallback.recommend_next(user, prev_track, prev_track_time)
+
+        previous_track = self.catalog.from_bytes(previous_track).track
+        embedding = self.user_embs[user] + self.context_embs[previous_track]
+        recommendations = np.argpartition(-np.dot(self.track_embs, embedding), 100)[:100].tolist()
+
+        shuffled = list(recommendations)
+        random.shuffle(shuffled)
+        return shuffled[0]
+
